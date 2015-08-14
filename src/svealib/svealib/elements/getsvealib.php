18ddef89127bf541a000b9d6e8305b0322612185
Anneli Halldn <anneli.halldn@sveaekonomi.se>
2015-08-14 12:49:57 +0200

    getsvealib contains version of module

--- src/svealib/svealib/elements/getsvealib.php
+++ src/svealib/svealib/elements/getsvealib.php
@@ -26,16 +26,18 @@ class JElementGetSvealib extends JElement {
 	 * @var        string
 	 */
 	var $_name = 'getSvealib';
+        var $sveaVersion = '2.4.20';
 
 	function fetchElement ($name, $value, &$node, $control_name) {
                 $update_url = "https://github.com/sveawebpay/virtuemart2-module/archive/master.zip";
                 $url = "https://raw.githubusercontent.com/sveawebpay/virtuemart2-module/master/docs/info.json";
                 $json = file_get_contents($url);
                 $data = json_decode($json);
+                $html .= "<div>Version: $this->sveaVersion</div><br />";
                 $html .= "<div>";
-                if($data->module_version <= JText::sprintf('VMPAYMENT_SVEA_VERSION')){
-                    $html .= "You have the latest ". $data->module_version . " version.";
-                }elseif ($data->module_version > JText::sprintf('VMPAYMENT_SVEA_VERSION')) {
+                if($data->module_version <= $this->sveaVersion){
+                    $html .= "You have the latest ". $this->sveaVersion . " version.";
+                }elseif ($data->module_version > $this->sveaVersion) {
                     $html .= "UPDATE FOUND! <br />".$data->module_version;
                     $img = '<img src="'.JURI::root ().'/plugins/vmpayment/svealib/assets/images/download.png" height="56" widh="56" />';
                     $html .= "&nbsp;<br /><a href='$update_url'>$img</a>";
